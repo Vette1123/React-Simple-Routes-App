0b982272a1a76fa1657ce5de8f7bdbde5a272d64
MohamedGado <boogado@yahoo.com>
2022-03-31 13:14:37 +0200

    task 1 react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Posts from "./pages/Posts";
+import Profile from "./pages/Profile";
+import Footer from "./components/Footer";
+import NavBarCom from "./components/NavBarCom";
+import UserProfileCom from "./components/UserProfileCom";
 
+import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarCom />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/:id" element={<UserProfileCom />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
